16e77444a43207e7b562164617f07afc4bdd5bbe
Hasina Akter <66660517+Hasina-Akter20@users.noreply.github.com>
2023-01-14 18:43:42 +0600

    complete

---
+++ script.js
@@ -0,0 +1,40 @@
+function cloudPlay() {
+  var cloud = document.getElementById("cloudPlay");
+  cloud.play();
+}
+function windPlay() {
+  console.log("wind");
+  var wind = document.getElementById("windPlay");
+  wind.play();
+}
+function everestPlay() {
+  console.log("everest");
+  var everest = document.getElementById("everestPlay");
+  everest.play();
+}
+function oceanPlay() {
+  console.log("ocean");
+  var ocean = document.getElementById("oceanPlay");
+  ocean.play();
+}
+function bricksPlay() {
+  console.log("Bricks falling");
+  var bricks = document.getElementById("bricksPlay");
+  bricks.play();
+}
+function waterfallPlay() {
+  console.log("Waterfall");
+  var waterfall = document.getElementById("waterfallPlay");
+  waterfall.play();
+}
+function beachPly() {
+  console.log("beach");
+  var beach = document.getElementById("beachPly");
+  beach.play();
+}
+
+//pause();
+
+// function onPause() {
+//   x.pause();
+// }
